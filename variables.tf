dd24deca4bc4f2770309103648bbd2cac876cd38
Andrew Simon <asimon@asimon.net>
2025-12-18 21:29:18 -0500

    use updated  AMI

--- variables.tf
+++ variables.tf
@@ -37,7 +37,7 @@ variable "instance_type" {
 }
 
 variable "ami_id" {
-  description = "Please subscribe to this TLC AMI ID for the EC2 instance before using."
+  description = "Please subscribe to this TLC AMI ID for the EC2 instance before using tf."
   type        = string
-  default = "ami-0424b6c6518fafbaf"
+  default = "ami-04398049c37e2ad5a"
 }
