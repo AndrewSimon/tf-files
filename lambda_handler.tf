c6663542a7ceac8ce4a1338b4eedccf604e12956
Andrew Simon <asimon@asimon.net>
2025-12-22 09:07:33 -0500

    change lambda url response mode

--- lambda_handler.tf
+++ lambda_handler.tf
@@ -321,6 +321,7 @@ resource "aws_lambda_function" "spot_runner" {
 
 resource "aws_lambda_function_url" "spot_lambda_url" {
   function_name      = aws_lambda_function.spot_runner.function_name
+  invoke_mode        = "RESPONSE_STREAM"
   authorization_type = "NONE" # Restrict access with 'AWS_IAM'
   cors {
     # Origins that can access the function URL
