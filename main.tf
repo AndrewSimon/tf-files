a349d4450a3852d3904a6395ed5d2c615ded332b
Andrew Simon <asimon@asimon.net>
2025-12-22 18:25:30 -0500

    enable ssm default management for acct

--- main.tf
+++ main.tf
@@ -162,6 +162,11 @@ resource "aws_instance" "tf-instance" {
   } 
 }
 
+resource "aws_ssm_service_setting" "default_host_management" {
+  setting_id    = "/ssm/managed-instance/default-ec2-instance-management-role"
+  setting_value = "spot-instance-role"
+}
+
 data "github_actions_registration_token" "spot_runner" {
   repository = "tf-files"
 }
