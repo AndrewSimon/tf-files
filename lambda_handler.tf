3180a779444084746f702c06454bcd1914a525b6
asimon <asimon@laptop>
2026-01-07 21:09:12 -0500

    lambda_handler.tf: user-data again

--- lambda_handler.tf
+++ lambda_handler.tf
@@ -61,7 +61,7 @@ GH_PAT = '${data.aws_ssm_parameter.gh_pat.value}'
 PROFILE_NAME = 'SysAdmin'
 SPOT_MARKET = ${var.spot_market}
 USERDATA = f"""#!/bin/bash
-export RUNNER_TOKEN=$(curl -s -L -X POST -H "Accept: application/vnd.github+json" -H "Authorization: Bearer {GH_PAT}" -H "X-GitHub-Api-Version: 2022-11-28" https://api.github.com/repos/AndrewSimon/tf-files/actions/runners/registration-token| grep token|awk -F\\" '{print $4}')
+export RUNNER_TOKEN=$(curl -s -L -X POST -H "Accept: application/vnd.github+json" -H "Authorization: Bearer {GH_PAT}" -H "X-GitHub-Api-Version: 2022-11-28" https://api.github.com/repos/AndrewSimon/tf-files/actions/runners/registration-token| grep token|awk -F\\" '{{print $4}}')
 sudo -u gh-runner bash -c "cd /home/gh-runner && ./config.sh remove --token $RUNNER_TOKEN"
 sudo -u gh-runner bash -c "cd /home/gh-runner && ./config.sh --url https://github.com/AndrewSimon/tf-files --token $RUNNER_TOKEN --unattended --replace --name tlc-spot-runner"
 nohup sudo -u gh-runner bash -c 'cd /home/gh-runner && ./run.sh' &
