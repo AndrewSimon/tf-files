b9f3a57045f69ce39c5077caba0fc58eef472db7
Andrew Simon <asimon@asimon.net>
2026-01-13 14:45:49 -0500

    variables.tf: New default AMI

--- variables.tf
+++ variables.tf
@@ -40,7 +40,7 @@ variable "instance_type" {
 variable "ami_id" {
   description = "Please subscribe to this TLC AMI ID for the spot instance before using tf."
   type        = string
-  default = "ami-0f12e4302d2ae8b8f"
+  default = "ami-0be2eb108b62c31a3"
 }
 
 variable "spot_market" {
